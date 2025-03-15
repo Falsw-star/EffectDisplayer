762bce52eb6f414bfe1292baa365f3c5a1ea14e5
Falsw-star <Falsestar114514@outlook.com>
2025-03-15 20:16:17 +0800

    init

---
+++ src/main/java/top/falsw/effectdisplayer/Effectdisplayer.java
@@ -0,0 +1,69 @@
+package top.falsw.effectdisplayer;
+
+import com.mojang.brigadier.Command;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.decoration.DisplayEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.Text;
+import net.minecraft.util.math.AffineTransformation;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
+import org.joml.Vector3f;
+
+public class Effectdisplayer implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+        // 这个项目用于测试通过 1.19 的 Minecraft 引入的 DisplayEntity 制作一些特效的想法
+        // 添加一些指令以便测试
+
+        // 生成手中物品
+        Command<ServerCommandSource> onhand = (context) -> {
+            ServerCommandSource source = context.getSource();
+            MinecraftServer server = source.getServer();
+            World world = source.getWorld();
+            PlayerEntity player = source.getPlayer();
+            Vec3d pos = source.getPosition().add(0, 1, 0).offset(player.getHorizontalFacing(), 3); // 使初始位置稍作移动以便观察
+
+            // 新建 ItemDisplayEntity
+            DisplayEntity.ItemDisplayEntity displayEntity = new DisplayEntity.ItemDisplayEntity(EntityType.ITEM_DISPLAY, world);
+            // 设置初始数据
+            displayEntity.setItemStack(player.getMainHandStack());
+            displayEntity.setPosition(pos);
+            displayEntity.setTransformation(new AffineTransformation(
+                    new Vector3f(0, 0, 0), // 平移
+                    null, // 旋转
+                    new Vector3f(1, 1, 1), // 缩放
+                    null // 旋转
+            ));
+            // 设置插值， 单位为 tick
+            displayEntity.setStartInterpolation(0);
+            displayEntity.setInterpolationDuration(100);
+            server.execute(() -> {
+                // 添加到世界
+                world.spawnEntity(displayEntity);
+                // 设置变换
+                displayEntity.setTransformation(new AffineTransformation(
+                        new Vector3f(0, 0, 0),
+                        null,
+                        new Vector3f(5, 0.2f, 5), // 压扁
+                        null
+                ));
+            });
+            source.sendFeedback(() -> Text.literal(
+                    "生成于 " + pos
+            ), false);
+            return 1;
+        };
+
+        // 注册指令
+        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+            dispatcher.register(CommandManager.literal("onhand").executes(onhand));
+        });
+    }
+}
---
+++ src/main/java/top/falsw/effectdisplayer/client/EffectdisplayerClient.java
@@ -0,0 +1,10 @@
+package top.falsw.effectdisplayer.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EffectdisplayerClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
